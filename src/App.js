2a233c3146aaa6b845c6ba37f7487ce81431a194
matebe12 <matebe12@gmail.com>
2023-01-29 02:29:35 +0900

    build test

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
   const [spanArr,setSpanArr]= useState([]);
   const [videoThumbnail,setThumbnails]= useState([]);
   const [seekValue, setSeekValue] = useState([0,0])
-  const gg = 'test'
+  const gg = 'tes'
 
 
   useEffect(()=> {
